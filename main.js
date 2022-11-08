f804f6277e615f58f1792a2e4d4b014be8362bac
Anastasia Sh <anastasia.shulman@hm.edu>
2022-11-08 11:01:28 +0100

    Made changes to start app on port 3000

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 import express from 'express'
+import path from 'path'
 
+const PORT = 3000;
+
+const __dirname = path.resolve();
+
+const app = express();
+
+const publicDir = path.join(__dirname, 'public');
+console.log(publicDir);
+console.log(__dirname);
+
+app.use('/', express.static(publicDir));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
+
+//app.get('/', (req))
